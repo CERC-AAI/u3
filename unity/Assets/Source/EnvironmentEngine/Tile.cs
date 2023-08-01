7b3b3ae5b6b7c64c3a6b8807117afe7853c2c58c
AndrewRWilliams <williams.andrew1305@gmail.com>
2023-08-01 16:38:35 -0400

    comment out CanPlaceTile()

--- unity/Assets/Source/EnvironmentEngine/Tile.cs
+++ unity/Assets/Source/EnvironmentEngine/Tile.cs
@@ -24,28 +24,28 @@ public class Tile : MonoBehaviour
     };
 
 
-    public bool CanPlaceTile(Tile[,,] environment, Vector3Int position)
-    {
-        // Check if the faces of the tile are compatible with the neighboring tiles according to the matching matrix
-        // Debug.Log("CanPlaceTile() position: " + position);
-        int[] neighborFaces = GetNeighborFaces(environment, position);
-        // Debug.Log("neighborFaces: " + neighborFaces[0] + " " + neighborFaces[1] + " " + neighborFaces[2] + " " + neighborFaces[3] + " " + neighborFaces[4] + " " + neighborFaces[5] + " ");
+    // public bool CanPlaceTile(Tile[,,] environment, Vector3Int position)
+    // {
+    //     // Check if the faces of the tile are compatible with the neighboring tiles according to the matching matrix
+    //     // Debug.Log("CanPlaceTile() position: " + position);
+    //     int[] neighborFaces = GetNeighborFaces(environment, position);
+    //     // Debug.Log("neighborFaces: " + neighborFaces[0] + " " + neighborFaces[1] + " " + neighborFaces[2] + " " + neighborFaces[3] + " " + neighborFaces[4] + " " + neighborFaces[5] + " ");
 
-        for (int i = 0; i < 6; i++)
-        {
-            if (neighborFaces[i] == -1) // no face in this direction
-            {
-                continue;
-            }
+    //     for (int i = 0; i < 6; i++)
+    //     {
+    //         if (neighborFaces[i] == -1) // no face in this direction
+    //         {
+    //             continue;
+    //         }
 
-            if (!matchingMatrix[faceIDs[i]].Contains(neighborFaces[i]))
-            {
-                return false;
-            }
-        }
+    //         if (!matchingMatrix[faceIDs[i]].Contains(neighborFaces[i]))
+    //         {
+    //             return false;
+    //         }
+    //     }
 
-        return true;
-    }
+    //     return true;
+    // }
 
     // three types of edges: ground, bedrock, air
     // air can attach to anything
@@ -57,43 +57,43 @@ public class Tile : MonoBehaviour
         mParentMetaTile = GetComponentInParent<MetaTile>();
     }
 
-    public static Tile GetNeighbor(Tile[,,] environment, int x, int y, int z)
-    {
-        if (x < 0 || x >= environment.GetLength(0) ||
-            y < 0 || y >= environment.GetLength(1) ||
-            z < 0 || z >= environment.GetLength(2))
-        {
-            return null;
-        }
+    // public static Tile GetNeighbor(Tile[,,] environment, int x, int y, int z)
+    // {
+    //     if (x < 0 || x >= environment.GetLength(0) ||
+    //         y < 0 || y >= environment.GetLength(1) ||
+    //         z < 0 || z >= environment.GetLength(2))
+    //     {
+    //         return null;
+    //     }
 
-        return environment[x, y, z];
-    }
+    //     return environment[x, y, z];
+    // }
 
-    public static int[] GetNeighborFaces(Tile[,,] environment, Vector3Int position)
-    {
-        int x = position.x;
-        int y = position.y;
-        int z = position.z;
+    // public static int[] GetNeighborFaces(Tile[,,] environment, Vector3Int position)
+    // {
+    //     int x = position.x;
+    //     int y = position.y;
+    //     int z = position.z;
 
-        // Get the appropriate int from the faceTypes field of the neighboring tiles in each direction
-        Tile[] neighbors = new Tile[6];
-        neighbors[0] = GetNeighbor(environment, x - 1, y, z);
-        neighbors[1] = GetNeighbor(environment, x + 1, y, z);
-        neighbors[2] = GetNeighbor(environment, x, y - 1, z);
-        neighbors[3] = GetNeighbor(environment, x, y + 1, z);
-        neighbors[4] = GetNeighbor(environment, x, y, z - 1);
-        neighbors[5] = GetNeighbor(environment, x, y, z + 1);
+    //     // Get the appropriate int from the faceTypes field of the neighboring tiles in each direction
+    //     Tile[] neighbors = new Tile[6];
+    //     neighbors[0] = GetNeighbor(environment, x - 1, y, z);
+    //     neighbors[1] = GetNeighbor(environment, x + 1, y, z);
+    //     neighbors[2] = GetNeighbor(environment, x, y - 1, z);
+    //     neighbors[3] = GetNeighbor(environment, x, y + 1, z);
+    //     neighbors[4] = GetNeighbor(environment, x, y, z - 1);
+    //     neighbors[5] = GetNeighbor(environment, x, y, z + 1);
 
-        int[] neighborFaces = new int[6];
-        neighborFaces[0] = neighbors[0] == null ? -1 : neighbors[0].faceIDs[1];
-        neighborFaces[1] = neighbors[1] == null ? -1 : neighbors[1].faceIDs[0];
-        neighborFaces[2] = neighbors[2] == null ? -1 : neighbors[2].faceIDs[3];
-        neighborFaces[3] = neighbors[3] == null ? -1 : neighbors[3].faceIDs[2];
-        neighborFaces[4] = neighbors[4] == null ? -1 : neighbors[4].faceIDs[5];
-        neighborFaces[5] = neighbors[5] == null ? -1 : neighbors[5].faceIDs[4];
+    //     int[] neighborFaces = new int[6];
+    //     neighborFaces[0] = neighbors[0] == null ? -1 : neighbors[0].faceIDs[1];
+    //     neighborFaces[1] = neighbors[1] == null ? -1 : neighbors[1].faceIDs[0];
+    //     neighborFaces[2] = neighbors[2] == null ? -1 : neighbors[2].faceIDs[3];
+    //     neighborFaces[3] = neighbors[3] == null ? -1 : neighbors[3].faceIDs[2];
+    //     neighborFaces[4] = neighbors[4] == null ? -1 : neighbors[4].faceIDs[5];
+    //     neighborFaces[5] = neighbors[5] == null ? -1 : neighbors[5].faceIDs[4];
 
-        return neighborFaces;
-    }
+    //     return neighborFaces;
+    // }
 
     // Draw a semitransparent red cube at the transforms position
     void OnDrawGizmos()
